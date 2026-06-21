e335f8ead3578828540d32e007424866e64d46b8
mleem97 <52848568+mleem97@users.noreply.github.com>
2026-06-21 23:43:06 +0000

    Refactored GregFacilityModule.GetRackCount to avoid FindObjectsOfType

--- src/PublicApi/Modules/GregFacilityModule.cs
+++ src/PublicApi/Modules/GregFacilityModule.cs
@@ -4,8 +4,28 @@ namespace gregCore.PublicApi.Modules;
 
 public sealed class GregFacilityModule
 {
+    private const int DEVICE_INDEX_RACKS = 2; // Index 0: servers, Index 1: switches, Index 2: racks
+
     private readonly GregApiContext _ctx;
     internal GregFacilityModule(GregApiContext ctx) => _ctx = ctx;
 
-    public int GetRackCount() => UnityEngine.Object.FindObjectsOfType<global::Il2Cpp.Rack>().Length;
+    public int GetRackCount()
+    {
+        try
+        {
+            var nm = global::Il2Cpp.NetworkMap.instance;
+            if (nm != null)
+            {
+                var devices = nm.GetNumberOfDevices();
+                if (devices != null && devices.Length > DEVICE_INDEX_RACKS)
+                {
+                    return devices[DEVICE_INDEX_RACKS];
+                }
+            }
+        }
+        catch { }
+
+        var objects = UnityEngine.Object.FindObjectsOfType<global::Il2Cpp.Rack>();
+        return objects != null ? objects.Length : 0;
+    }
 }

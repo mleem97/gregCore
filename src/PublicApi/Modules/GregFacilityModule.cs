081b9a277fcc84d4c0559a8b51b5ca2cb8f8bd68
mleem97 <52848568+mleem97@users.noreply.github.com>
2026-07-02 23:53:43 +0000

    ⚡ Bolt: Optimize GetRackCount to O(1) in GregFacilityModule

--- src/PublicApi/Modules/GregFacilityModule.cs
+++ src/PublicApi/Modules/GregFacilityModule.cs
@@ -7,5 +7,20 @@ public sealed class GregFacilityModule
     private readonly GregApiContext _ctx;
     internal GregFacilityModule(GregApiContext ctx) => _ctx = ctx;
 
-    public int GetRackCount() => UnityEngine.Object.FindObjectsOfType<global::Il2Cpp.Rack>().Length;
+    private const int DEVICE_INDEX_RACKS = 2;
+
+    public int GetRackCount()
+    {
+        if (global::Il2Cpp.NetworkMap.instance != null)
+        {
+            var counts = global::Il2Cpp.NetworkMap.instance.GetNumberOfDevices();
+            if (counts != null && counts.Length > DEVICE_INDEX_RACKS)
+            {
+                return counts[DEVICE_INDEX_RACKS];
+            }
+        }
+
+        var racks = UnityEngine.Object.FindObjectsOfType<global::Il2Cpp.Rack>();
+        return racks != null ? racks.Length : 0;
+    }
 }

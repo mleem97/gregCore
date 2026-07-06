75f3d42096fe11f934bc266662e1a50b65abca38
mleem97 <52848568+mleem97@users.noreply.github.com>
2026-07-06 00:03:23 +0000

    Optimize GetRackCount to use O(1) NetworkMap lookup

--- src/PublicApi/Modules/GregFacilityModule.cs
+++ src/PublicApi/Modules/GregFacilityModule.cs
@@ -7,5 +7,18 @@ public sealed class GregFacilityModule
     private readonly GregApiContext _ctx;
     internal GregFacilityModule(GregApiContext ctx) => _ctx = ctx;
 
-    public int GetRackCount() => UnityEngine.Object.FindObjectsOfType<global::Il2Cpp.Rack>().Length;
+    private const int DEVICE_INDEX_RACKS = 2; // 0 = servers, 1 = switches, 2 = racks
+
+    public int GetRackCount()
+    {
+        if (global::Il2Cpp.NetworkMap.instance != null)
+        {
+            var counts = global::Il2Cpp.NetworkMap.instance.GetNumberOfDevices();
+            if (counts != null && counts.Length > DEVICE_INDEX_RACKS)
+            {
+                return counts[DEVICE_INDEX_RACKS];
+            }
+        }
+        return UnityEngine.Object.FindObjectsOfType<global::Il2Cpp.Rack>().Length;
+    }
 }

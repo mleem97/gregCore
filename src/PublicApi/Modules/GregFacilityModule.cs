75faabcd368721930edc4dade399a651c703e744
mleem97 <52848568+mleem97@users.noreply.github.com>
2026-07-13 23:39:50 +0000

    Optimize GetRackCount to use NetworkMap.instance instead of FindObjectsOfType

--- src/PublicApi/Modules/GregFacilityModule.cs
+++ src/PublicApi/Modules/GregFacilityModule.cs
@@ -7,5 +7,20 @@ public sealed class GregFacilityModule
     private readonly GregApiContext _ctx;
     internal GregFacilityModule(GregApiContext ctx) => _ctx = ctx;
 
-    public int GetRackCount() => UnityEngine.Object.FindObjectsOfType<global::Il2Cpp.Rack>().Length;
+    // Index 0: servers, Index 1: switches, Index 2: racks
+    private const int DEVICE_INDEX_RACKS = 2;
+
+    public int GetRackCount()
+    {
+        if (global::Il2Cpp.NetworkMap.instance != null)
+        {
+            var counts = global::Il2Cpp.NetworkMap.instance.GetNumberOfDevices();
+            if (counts != null && counts.Length > DEVICE_INDEX_RACKS)
+            {
+                return counts[DEVICE_INDEX_RACKS];
+            }
+        }
+
+        return UnityEngine.Object.FindObjectsOfType<global::Il2Cpp.Rack>().Length;
+    }
 }
